294df28334430139a81bd5126bf0806d22093ffb
limq <limq@cgnfex.com>
2020-10-22 15:57:20 +0800

    json

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class TestController extends Controller
 {
     //
+    public function testList()
+    {
+        $data = [
+            ["name" => "张三", "age" => 18, "sex" => "男"],
+            ["name" => "刘备", "age" => 18, "sex" => "男"],
+            ["name" => "关羽", "age" => 18, "sex" => "男"],
+            ["name" => "张飞", "age" => 18, "sex" => "男"]
+        ];
+        return new JsonResponse(["status_code" => 200, "message" => "success", "data" => $data]);
+    }
 }
